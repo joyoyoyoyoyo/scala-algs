099de87457b41aaefe7e9eae87ebe69dbc28f375
Angel Ortega <cs.angelortega@gmail.com>
2017-11-18 03:41:29 -0800

    Print Staircase

---
+++ src/main/scala/hackerrank/warmup/Staircase.scala
@@ -0,0 +1,9 @@
+package hackerrank.warmup
+
+object Staircase {
+  val n = io.StdIn.readInt()
+  val staircase = (1 to n).foldLeft("")((line, num) => line + " " * (n - num) + "#" * num + "\n" )
+
+  println(staircase)
+
+}
