0f3155a0e984d559a9ea3014033d7f80ae9541a4
Angel Ortega <cs.angelortega@gmail.com>
2017-11-18 21:40:52 -0800

    Attempt at functional attempt for Problem 1, however there is a match error

--- src/main/scala/ctci/ArraysAndStrings.scala
+++ src/main/scala/ctci/ArraysAndStrings.scala
@@ -1,6 +1,21 @@
-package ctci
-
 object ArraysAndStrings {
 
-  def isUniqueChars()
+
+  def isUniqueChars(str: String): Boolean = {
+    val charSet = Array.fill(128)(false)
+
+    @scala.annotation.tailrec
+    def loop(str: Seq[Char], truthy: Boolean): Boolean = {
+      str match {
+        case Nil => true
+        case Seq(c) if charSet(c.toInt) => false
+        case Seq(c) => loop(str.tail, !charSet(c.toInt))
+      }
+    }
+
+    val arr = str.toCharArray.toSeq
+    loop(arr, false)
+  }
+  val uniqueness = isUniqueChars("abc")
+  println(s"uniqueness=${uniqueness}")
 }
