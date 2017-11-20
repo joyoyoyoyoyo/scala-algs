e2710609a134d3c73119f1ec23f9a6f4d674d1a3
Angel Ortega <cs.angelortega@gmail.com>
2017-11-19 20:44:39 -0800

    Clean solution for Birthday Cake Candles

--- src/main/scala/hackerrank/warmup/BirthdayCakeCandles.scala
+++ src/main/scala/hackerrank/warmup/BirthdayCakeCandles.scala
@@ -2,4 +2,20 @@ package hackerrank.warmup
 
 object BirthdayCakeCandles {
 
+  case class BlownCandle(frequency: Int, tallest: Int)
+
+  def birthdayCakeCandles(n: Int, arr: Array[Int]): Int =  {
+    // Complete this function
+    val blownOut = arr.foldLeft(BlownCandle(0,0))((candle, currentCandleHeight) => {
+      if(currentCandleHeight > candle.tallest)
+        BlownCandle(1, currentCandleHeight)
+      else if (currentCandleHeight == candle.tallest){
+        BlownCandle(candle.frequency + 1, candle.tallest)
+      } else {
+        BlownCandle(candle.frequency, candle.tallest)
+      }
+    })
+    blownOut.frequency
+  }
+
 }
