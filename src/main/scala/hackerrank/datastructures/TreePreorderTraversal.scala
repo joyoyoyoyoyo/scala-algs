18e2a054c8597e749a8756e1ca5e5e57b6c9c415
Angel Ortega <cs.angelortega@gmail.com>
2017-11-19 23:37:13 -0800

    Add preorder traversal

--- src/main/scala/hackerrank/datastructures/TreePreorderTraversal.scala
+++ src/main/scala/hackerrank/datastructures/TreePreorderTraversal.scala
@@ -1,5 +1,23 @@
 package hackerrank.datastructures
 
-object TreePreorderTraversal {
+import scala.annotation.tailrec
 
+abstract class Tree[+A]
+case class Branch[+A](value: A, left: Tree[A], right: Tree[A]) extends Tree[A]
+case object Leaf extends Tree[Nothing]
+
+
+object Tree {
+
+
+
+  def preOrder[T](root : Tree[T]): Tree[T] = {
+
+    root match {
+      case Branch(value, left, right) => { println(value); preOrder(left); preOrder(right) }
+      case Leaf => None
+    }
+    root
+  }
+  preOrder(Branch(1, Branch(2, Branch(8, Leaf, Leaf), Branch(5, Leaf, Leaf)), Branch(-2, Branch(-100, Leaf, Leaf), Leaf)))
 }
