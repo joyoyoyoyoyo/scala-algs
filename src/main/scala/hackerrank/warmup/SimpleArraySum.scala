12886a24f2be9089c60821cf5fc8ec9e3f333ca2
Angel Ortega <cs.angelortega@gmail.com>
2017-11-19 18:40:51 -0800

    Documentation and cleanup

--- src/main/scala/hackerrank/warmup/SimpleArraySum.scala
+++ src/main/scala/hackerrank/warmup/SimpleArraySum.scala
@@ -3,19 +3,21 @@ import scala.util._
 
 object SimpleArraySum  {
 
+  /** Two sum methods **/
+  def simpleArraySum(n: Int, arr: Array[Int]) = arr.sum
+  def foldSum(n: Int, arr: Array[Int]) = (0 until n).fold(0)(_ + arr(_))
+
+  /** Read in an array **/
   val n = io.StdIn.readInt()
   val arr = io.StdIn.readLine().split(" ").map(acc => Try(acc.toInt).getOrElse(0))
 
+  /** Sum the array values **/
+  val result = simpleArraySum(n, arr)
 
-  val result = Solution.foldSum(n, arr)
+
+  /** Check equality and associativity for sum **/
   assert(arr.sum == result, s"Sum equality is not correct the result should be ${arr.sum}, not $result")
   assert(arr.sum + 1 == result + 1)
   println(s"Sum: $result with time complexity of O(n) and O(n) memory storage," +
     s" where n is the size of the array")
-}
-
-object Solution {
-  def simpleArraySum(n: Int, arr: Array[Int]) = arr.sum
-
-  def foldSum(n: Int, arr: Array[Int]) = (0 until n).fold(0)(_ + arr(_))
 }
