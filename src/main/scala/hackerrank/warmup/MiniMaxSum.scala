fbaa903be607e103b7e338751282734b2e046761
Angel Ortega <cs.angelortega@gmail.com>
2017-11-19 18:33:57 -0800

    Find min and Max

--- src/main/scala/hackerrank/warmup/MiniMaxSum.scala
+++ src/main/scala/hackerrank/warmup/MiniMaxSum.scala
@@ -1,8 +1,23 @@
 package hackerrank.warmup
 
-object MiniMaxSum {
+object MiniMaxSum extends App{
 
-  val arr = io.Source.stdin.getLines().next().split(" ").map(_.toInt)
-  val total = arr.foldLeft(0)(_ + _)
-  ???
+  val arr = Array(6, 5, 4, 5)
+  def minMaxSum(arr: Array[Int]) = {
+    require(arr.nonEmpty, "Empty Array Passed")
+    val result = (arr).foldLeft((0, Integer.MAX_VALUE, Integer.MIN_VALUE)) { (acc, element) => {
+      val (total, min, max) = acc
+      (total, min, max) match {
+        case (x, y, z) if element > max => (total + element, min, element)
+        case (x, y, z) if element < min => (total + element, element, max)
+        case (x, y, z) => (total + element, min, max)
+      }}
+    }
+
+    (result._2, result._3)
+  }
+
+  val (min,max) = minMaxSum(arr)
+  println(s"min=$min, max=$max")
+
 }
