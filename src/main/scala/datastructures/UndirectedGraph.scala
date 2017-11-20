be03bfac7c90b1da2ef86ca09d5548b12ed4b4a0
Angel Ortega <cs.angelortega@gmail.com>
2017-11-19 19:11:11 -0800

    Adding Graph Requirements

---
+++ src/main/scala/datastructures/UndirectedGraph.scala
@@ -0,0 +1,18 @@
+package datastructures
+
+/**
+  * We have two possible representations for a graph:
+  *
+  *   1. Adjaceny List
+  *   2. Matrix
+  *
+  * For the purposes of getting started (and to get comfortable with Array operations) we will use a matrix.
+  *
+  *   (Discuss advantages and disadvantages here)
+  *
+  */
+
+
+object UndirectedGraph {
+
+}
