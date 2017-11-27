cde3cb2eda6d2e177d0881c0ecc24bc21476c808
Angel Ortega <cs.angelortega@gmail.com>
2017-11-27 12:51:47 -0800

    Move Union Find to Data structures package

--- src/main/scala/datastructures/UF.scala
+++ src/main/scala/datastructures/UF.scala
@@ -1,6 +1,6 @@
-package hackerrank.datastructures
+package datastructures
 
-abstract class UF(val forest: Array[Int]) {
+abstract case class UF(val forest: Array[Int]) {
   forest.indices.map(idx => forest(idx) = idx)
   abstract def union(p: Int, q: Int): Unit
   abstract def connected(p: Int, q: Int): Boolean
