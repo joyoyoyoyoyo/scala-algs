f52d48378415179c7c312818880d7e5c24c9dbdb
Angel Ortega <cs.angelortega@gmail.com>
2017-12-12 08:19:08 -0800

    Writing design for pattern match: either use regex or extractor pattern (prefer extractor pattern)

--- src/main/scala/hackerrank/algorithms/warmup/TimeConversion.scala
+++ src/main/scala/hackerrank/algorithms/warmup/TimeConversion.scala
@@ -1,5 +1,23 @@
 package hackerrank.algorithms.warmup
 
+case class MilitaryTime()
+
 object TimeConversion {
+
+  val militaryTimeRegex = ???
+  val twelveHourClockRegex = ???
+
+  def timeConversion[String](twelveHourClock: String): String =  {
+
+    val militaryTime = twelveHourClock match {
+      case miltaryTimeRegex => ???
+      case twelveHourClockRegex => ???
+    }
+    militaryTime
+  }
+
+  def main(args: Array[String]) = {
+    val military0 = "07:05:45PM".map(timeConversion)
+  }
 
 }
