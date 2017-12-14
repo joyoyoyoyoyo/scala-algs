a98734eb6c4fe003ae0afd0668d10004387e479d
Angel Ortega <cs.angelortega@gmail.com>
2017-12-13 18:19:30 -0800

    make public methods and variables show their type

--- src/main/scala/datastructures/QuickFindUF.scala
+++ src/main/scala/datastructures/QuickFindUF.scala
@@ -1,18 +1,20 @@
+package datastructures
+
 /** Read in the number of nodes in the forest for the disjoint sets
   * 
   **/
 class QuickFindUF(val n: Int) {
-  val id = Array.tabulate(n)(index => index)
+  val id: Array[Int] = Array.tabulate(n)(index => index)
   
   /** O(1) algorithm to check if two components are connected
     *   for the Dynamic-Connectivity problem
     **/
-  def connected(p: Int, q: Int) = id(p) == id(q)
+  def connected(p: Int, q: Int): Boolean = id(p) == id(q)
 
   /** O(n) algorithm to merge two connected components
     *
     **/
-  def union(p: Int, q: Int) = {
+  def union(p: Int, q: Int): Unit = {
     val pid = id(p)
     val qid = id(q)
     for (i <- id.indices) {
