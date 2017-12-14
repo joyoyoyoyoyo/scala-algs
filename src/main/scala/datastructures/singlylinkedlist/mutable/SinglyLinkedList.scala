0b3c9bcc04031c6124fd9c9ade97187a3c94e675
Angel Ortega <cs.angelortega@gmail.com>
2017-12-13 22:04:19 -0800

    Find Kth implemented in the linkedlist

--- src/main/scala/datastructures/singlylinkedlist/mutable/SinglyLinkedList.scala
+++ src/main/scala/datastructures/singlylinkedlist/mutable/SinglyLinkedList.scala
@@ -7,7 +7,11 @@ object SinglyLinkedList {
 
     var head: Node = Nil
 
-    def remove(v: Int) = {
+    /**
+      * O(n) algorithm
+      * @param v
+      */
+    def remove(v: Int): Unit = {
       head = head match {
         case Nil => Nil
         case Cons(vv, Nil) if vv == v => Nil
@@ -26,9 +30,23 @@ object SinglyLinkedList {
       }
     }
 
+    /**
+      * O(n) algorithm because of traversal from head to the kth node
+      * @param index
+      * @return
+      */
+    def findKth(index: Int): Int = {
+      require(index > 0)
+      findKthHelper(index, head)
+    }
 
-
-    def findKth(index: Int): Int = ???
+    private def findKthHelper(index: Int, n: Node): Int = {
+      (index, n) match {
+        case (x, Nil) => -1
+        case (i, xs: Cons) if i == 0 => xs.v
+        case (i, xs: Cons) => findKthHelper(i - 1, xs.next)
+      }
+    }
 
     /** Create a new node and insert the new node into the head, with the next pointer
       * pointing to the rest of the linked list
