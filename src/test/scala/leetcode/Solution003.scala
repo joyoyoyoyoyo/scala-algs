7fec49274c936136ba6c0d2ee121c7a481a7fd02
Angel Ortega <cs.angelortega@gmail.com>
2018-02-04 11:32:07 -0800

    Fixture for Test003

---
+++ src/test/scala/leetcode/Solution003.scala
@@ -0,0 +1,14 @@
+package leetcode
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/4/2018
+  */
+class Solution003 extends FunSuite with Matchers {
+  test("") {
+
+  }
+
+}
