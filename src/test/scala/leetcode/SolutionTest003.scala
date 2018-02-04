f1a0a7038470cfad69938849bdb7b7731c553821
Angel Ortega <cs.angelortega@gmail.com>
2018-02-04 12:27:23 -0800

    Add new vertical 'leetcode' using TDD

---
+++ src/test/scala/leetcode/SolutionTest003.scala
@@ -0,0 +1,19 @@
+package leetcode
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/4/2018
+  */
+class SolutionTest003 extends FunSuite with Matchers {
+
+  test("given a string, the length of the longest substring without repeating characters should be returned") {
+
+    assert(Solution003.lengthOfLongestSubstring("abcabcbb") == 3)
+    assert(Solution003.lengthOfLongestSubstring("bbbbbb") == 1)
+    assert(Solution003.lengthOfLongestSubstring("pwwkew") == 3)
+
+  }
+
+}
