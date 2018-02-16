f8325eb8731d6a1d45077b166877ed91e994bef2
Angel Ortega <cs.angelortega@gmail.com>
2018-02-16 14:41:25 -0800

    Test written for InsertionSort

---
+++ src/main/scala/algorithms/InsertionSort.scala
@@ -0,0 +1,14 @@
+package algorithms
+
+import scala.language.higherKinds
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/16/18
+  */
+object InsertionSort {
+
+  def sort(unsorted: List[Int]) = ???
+
+
+}
