8d3e1d2081b2655733a87a36a25739dd969bf6b1
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 14:54:53 -0800

    Debugging Insertion sort

--- src/main/scala/algorithms/sorting/InsertionSort.scala
+++ src/main/scala/algorithms/sorting/InsertionSort.scala
@@ -1,37 +1,34 @@
-//package algorithms.sorting
-//
-//import scala.language.higherKinds
-//
-///**
-//  * Created by: Angel Ortega
-//  * Date: 2/16/18
-//  */
-//object InsertionSort {
-//
-//  @scala.annotation.tailrec
-//  def isort[A <: Ordered[A]](x: A, unsorted: List[A]): List[A] = {
-//    unsorted match {
-//      case head :: Nil => head :: Nil
-//      case next :: tail if x < next => isort(x, tail)
-//      case next :: tail if x > next => isort(next, next :: x :: tail)
-//    }
+package algorithms.sorting
+
+import scala.language.higherKinds
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/16/18
+  */
+object InsertionSort {
+
+//  def search[A : Ordering](acc: List[A], element: A): List[A] = {
+//    import scala.math.Ordering.Implicits._
+//    acc.indices.reduceLeft( (i, j) => acc(i) < acc(j))
 //  }
-//
-//  @scala.annotation.tailrec
-//  def sortLoop[A <: Ordered[A]](sorted: List[A], unsorted: List[A]): List[A] = {
-//    unsorted match {
-//      case Nil => Nil
-//      case head :: Nil => head :: Nil
-//      case head :: tail => sortLoop(isort(head, sorted), tail)
-//    }
-//  }
-//
-//  def sort[A <: Ordered[A]](unsorted: List[A]) = {
-//    unsorted match {
-//      case Nil => Nil
-//      case head :: Nil => head :: Nil
-//      case head :: tail => sortLoop(isort(head, head :: tail), tail)
-//    }
-//  }
-//
-//}
+
+  def sort[A : Ordering](unsorted: List[A]): List[A] = {
+    import scala.math.Ordering.Implicits._
+
+    val sortedLL = unsorted.indices.foldLeft(List.empty[A])( (acc, i) =>{
+      val indexOfMin: Int = (i until unsorted.length).reduceLeft(
+        (x, y) =>
+          if (unsorted(x) < unsorted(y)) x else y
+      )
+      val swap = unsorted(i)
+      val firstSwapLL = unsorted.updated(i, unsorted(indexOfMin))
+      firstSwapLL.updated(indexOfMin, swap)
+
+    })
+    sortedLL
+  }
+
+
+
+}
