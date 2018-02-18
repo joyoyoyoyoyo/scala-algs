5b88eef09cc4c19d940819b23d89f50cfb749cdc
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 15:11:39 -0800

    Old attempt at Leetcode problem 3

--- src/main/scala/leetcode/Solution003.scala
+++ src/main/scala/leetcode/Solution003.scala
@@ -8,25 +8,29 @@ import java.nio.StringCharBuffer
   */
 object Solution003 {
   def lengthOfLongestSubstring(s: String): Int = {
-    var i = 0
-    val dictionary = Array.fill(128)(-1)
+    val dictionary = Array.fill(128)((-1,0))
     slideOnString(s, 0, 0, dictionary, 0)
 
 
   }
 
-  def slideOnString(s: String, start: Int, next: Int, dictionary: Array[Int], max: Int): Int = {
-    if (next == s.length)
-      max
+  @scala.annotation.tailrec
+  def slideOnString(s: String, start: Int, next: Int, dictionary: Array[(Int,Int)], max: Int): Int = {
+    if (next == s.length) {
+      dictionary.foldLeft(next - start)((maxSoFar, tuple) => {
+        math.max(tuple._2, maxSoFar)
+      })
+    }
     // base case: Check if exists
-    else if (dictionary(s(next)) == -1) {
-      dictionary(s(next)) = next
+    else if (dictionary(s(next))._1 == -1) {
+      dictionary(s(next)) = (next, max + 1)
       slideOnString(s, start, next + 1, dictionary, max + 1)
     }
     else {
-      val offset = dictionary(s(next))
-      dictionary(s(next)) = next
-      math.max(slideOnString(s, next, next + 1, dictionary, next - (start + 1)), max)
+      val offset = dictionary(s(start))._1 + 1
+      val index = if (s(next) == s(offset)) next else offset
+      dictionary(s(next)) = (next, max )
+      slideOnString(s, index , next + 1, dictionary, next - offset)
     }
   }
 }
