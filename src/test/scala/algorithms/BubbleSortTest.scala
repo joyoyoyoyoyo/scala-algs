c8f7ece8707f0b66968aa442fbd31d47c161130b
Angel Ortega <cs.angelortega@gmail.com>
2018-02-17 21:59:42 -0800

    Bubble sort test

--- src/test/scala/algorithms/BubbleSortTest.scala
+++ src/test/scala/algorithms/BubbleSortTest.scala
@@ -1,6 +1,7 @@
 package algorithms
 
 import org.scalatest.{FlatSpec, Matchers}
+import algorithms.sorting.BubbleSort
 
 /**
   * Created by: Angel Ortega
@@ -8,6 +9,6 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class BubbleSortTest extends FlatSpec with Matchers {
   "[1, 5, 3, 4, 0, 8]" should "be sorted to [0, 1, 3, 4, 5, 8]" in {
-    fail()
+    assert(Array(0, 1, 3, 4, 5, 8) == BubbleSort.sort[Int](Array(1, 5, 3, 4, 0, 8), _ < _))
   }
 }
