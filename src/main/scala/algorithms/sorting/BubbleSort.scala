c385ec058c020e3755edd3e0a2ac38229ba0334c
Angel Ortega <cs.angelortega@gmail.com>
2018-02-17 23:51:00 -0800

    Bubble Sort implemente in-place, stable, comparison, internal, non-recursive  method

--- src/main/scala/algorithms/sorting/BubbleSort.scala
+++ src/main/scala/algorithms/sorting/BubbleSort.scala
@@ -9,8 +9,9 @@ object BubbleSort {
   def sort[T](arr: Array[T], cmp: (T, T) => Boolean): Array[T] = {
 
     for (i <- arr.indices) {
-      for (window <- (i until arr.length).sliding(2, 1)) {
-        if (cmp(arr(window(0)), arr(window(1)))) {
+      for (window <- (0 until arr.length).sliding(2, 1)) {
+        val x = arr(window(0))
+        if (cmp(arr(window(1)), arr(window(0)))) {
           val swap = arr(window(1))
           arr(window(1)) = arr(window(0))
           arr(window(0)) = swap
