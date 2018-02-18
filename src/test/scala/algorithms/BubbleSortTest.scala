6fc59e3556142082ab8e8bc4cb50f3609f004826
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 01:11:24 -0800

    Testing empty list, two-element list, and unchanging list for Bubble sort

--- src/test/scala/algorithms/BubbleSortTest.scala
+++ src/test/scala/algorithms/BubbleSortTest.scala
@@ -15,4 +15,20 @@ class BubbleSortTest extends FlatSpec with Matchers {
   "[1, 5, 3, 4, 0, 8, -12]" should "be sorted to [-12, 0, 1, 3, 4, 5, 8] and work on odd ranges" in {
     assert(Array(-12, 0, 1, 3, 4, 5, 8) sameElements BubbleSort.sort[Int](Array(1, -12, 5, 3, 4, 0, 8), _ < _))
   }
+
+  "[3, 1]" should "be sorted to [1, 3] and swap once" in {
+    assert(Array(1, 3) sameElements BubbleSort.sort[Int](Array(3, 1), _ < _))
+  }
+
+  "[1]" should "be sorted to [1] and not change" in {
+    assert(Array(1) sameElements BubbleSort.sort[Int](Array(1), _ < _))
+  }
+
+  "[1, 3]" should "be sorted to [1, 3] and not change" in {
+    assert(Array(1, 3) sameElements BubbleSort.sort[Int](Array(1, 3), _ < _))
+  }
+
+  "[]" should "be sorted to [] and not change" in {
+    assert(Array() sameElements BubbleSort.sort[Int](Array(), _ < _))
+  }
 }
