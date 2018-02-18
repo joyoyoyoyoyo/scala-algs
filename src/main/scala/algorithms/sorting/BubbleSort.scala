120e68ece95c8e47bdf2e8eccabe54b2efa04d70
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 01:12:43 -0800

    Bubble sort changed to operate greedily, instead of using brute-force

--- src/main/scala/algorithms/sorting/BubbleSort.scala
+++ src/main/scala/algorithms/sorting/BubbleSort.scala
@@ -1,6 +1,7 @@
 package algorithms.sorting
 
-/**
+/** Bubble Sort has a worst-case time-complexity of Big-O(n ** 2)
+  *
   * Created by: Angel Ortega
   * Date: 2/17/18
   */
@@ -8,8 +9,8 @@ object BubbleSort {
 
   def sort[T](arr: Array[T], cmp: (T, T) => Boolean): Array[T] = {
 
-    for (i <- arr.indices) {
-      for (window <- (0 until arr.length).sliding(2, 1)) {
+    for (i <- 1 until arr.length) {
+      for (window <- (0 to arr.length - i).sliding(2, 1)) {
         val x = arr(window(0))
         if (cmp(arr(window(1)), arr(window(0)))) {
           val swap = arr(window(1))
