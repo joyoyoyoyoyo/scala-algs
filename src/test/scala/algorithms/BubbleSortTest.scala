a44b14bc86de7336b399846d62302adf2ccf1f31
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 00:32:54 -0800

    Test odd number array for bubble sort

--- src/test/scala/algorithms/BubbleSortTest.scala
+++ src/test/scala/algorithms/BubbleSortTest.scala
@@ -11,4 +11,8 @@ class BubbleSortTest extends FlatSpec with Matchers {
   "[1, 5, 3, 4, 0, 8]" should "be sorted to [0, 1, 3, 4, 5, 8]" in {
     assert(Array(0, 1, 3, 4, 5, 8) sameElements BubbleSort.sort[Int](Array(1, 5, 3, 4, 0, 8), _ < _))
   }
+
+  "[1, 5, 3, 4, 0, 8, -12]" should "be sorted to [-12, 0, 1, 3, 4, 5, 8] and work on odd ranges" in {
+    assert(Array(-12, 0, 1, 3, 4, 5, 8) sameElements BubbleSort.sort[Int](Array(1, -12, 5, 3, 4, 0, 8), _ < _))
+  }
 }
