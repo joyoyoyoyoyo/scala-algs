a8b9649a6e03f38c86be36bcfbf252918b246a82
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 15:10:29 -0800

    Move sorting tests into algorithms.sorting, rather than algorithms

---
+++ src/test/scala/algorithms/sorting/InsertionSortTest.scala
@@ -0,0 +1,16 @@
+package algorithms.sorting
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/16/18
+  */
+class InsertionSortTest extends FlatSpec with Matchers {
+
+  "An unsorted array" should "sort in O(n^2)" in {
+    val unsorted = List(-5, 1, 2, 10, 5, -10, 100, 0, -3)
+    assert(InsertionSort.sort(unsorted)== List(-10, -5, -3, 0, 1, 2, 5, 10, 100))
+  }
+
+}
