f218c7877df4bc4151f77cc358645d3376cf3af7
Angel Ortega <cs.angelortega@gmail.com>
2018-02-18 14:55:23 -0800

    Adjust Sorting test for InsertionSort

--- src/test/scala/algorithms/InsertionSortTest.scala
+++ src/test/scala/algorithms/InsertionSortTest.scala
@@ -1,22 +1,17 @@
-//package algorithms
-//
-//import algorithms.sorting.InsertionSort
-//import org.scalatest.{FlatSpec, Matchers}
-//
-///**
-//  * Created by: Angel Ortega
-//  * Date: 2/16/18
-//  */
-//class InsertionSortTest extends FlatSpec with Matchers {
-//
-//  private val expectedSortings = Map(
-//    List(-5, 1, 2, 10, 5, -10, 100, 0, -3) -> List(-10, -5, -3, 0, 1, 2, 5, 10, 100)
-//  )
-//
-//  "An unsorted array" should "sort in O(n^2)" in {
-//    val unsorted = List(-5, 1, 2, 10, 5, -10, 100, 0, -3)
-//    val sortedActual = InsertionSort.sort(unsorted)
-//    assert(expectedSortings(unsorted) == sortedActual)
-//  }
-//
-//}
+package algorithms
+
+import algorithms.sorting.InsertionSort
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/16/18
+  */
+class InsertionSortTest extends FlatSpec with Matchers {
+
+  "An unsorted array" should "sort in O(n^2)" in {
+    val unsorted = List(-5, 1, 2, 10, 5, -10, 100, 0, -3)
+    assert(InsertionSort.sort(unsorted)== List(-10, -5, -3, 0, 1, 2, 5, 10, 100))
+  }
+
+}
