b3447e12741c293dd86b1ee5b10b17857785b29e
Angel Ortega <cs.angelortega@gmail.com>
2018-03-08 00:28:51 -0800

    Leetcode solution 3 for longestSubstring using functional programming

--- src/main/scala/leetcode/Solution003.scala
+++ src/main/scala/leetcode/Solution003.scala
@@ -13,37 +13,21 @@ package leetcode
   * Date: 2/4/2018
   */
 object Solution003 {
-  def lengthOfLongestSubstring0(s: String): Int = {
-    lengthOfLongestSubstringLoop0(s.toList, Map.empty[Char,Int], 0, 0, 0)
-  }
-
-  def lengthOfLongestSubstringLoop0(s: List[Char], map: Map[Char, Int], j: Int, i: Int, max: Int): Int = {
-    s match {
-      case Nil => max
-      case head :: tail => {
-        val newIndex = map.getOrElse(head, i)
-        lengthOfLongestSubstringLoop0(tail, map + (head -> (j + 1)), j + 1, newIndex, math.max(j - newIndex + 1, max))
-      }
-    }
-  }
-
   def lengthOfLongestSubstring(s: String): Int = {
-
-    val map = collection.mutable.Map.empty[Char, Int]
-
-    case class Substring(start: Int, end: Int)
-    val maxSubstring = s.zipWithIndex.foldLeft((Substring(0, 0), 0))( (subMax, strI) => {
-      val (character, i) = strI
-      val (sub, max) = subMax
-      val initialIndex = map.getOrElse(character, sub.start)
-      val ret = if (map.contains(character))
-        (Substring(initialIndex + 1, i), math.max(i - sub.start, max - 1))
-      else
-        (Substring(initialIndex, i), max + 1)
-      map += (character -> i)
-      ret
-    })
-    maxSubstring._2
+    def lengthOfLongestSubstringLoop(xs: String, map: Map[Char, Int], i: Int, j: Int, max: Int): Int = {
+      if (j >= xs.length ) max
+      else {
+          map.get(xs(j)) match {
+            case Some(index) => {
+              val substring = s.substring(i, j)
+              lengthOfLongestSubstringLoop(xs, map + (xs(j) -> j), index + 1, j + 1, math.max(j - i , max))
+            }
+            case None => lengthOfLongestSubstringLoop(xs, map + (xs(j) -> j), i, j + 1, math.max(j - i + 1, max))
+          }
+        }
+    }
+    lengthOfLongestSubstringLoop(s, Map.empty[Char,Int], 0, 0, 0)
   }
+
 
 }
