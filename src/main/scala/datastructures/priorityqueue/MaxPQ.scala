62cb0e0844589bf09c03eb18fd168b3b2c8ad595
Angel Ortega <cs.angelortega@gmail.com>
2018-03-07 19:33:01 -0800

    Data Structures > Priority Queue > Binary Heap > 'Compiling'

--- src/main/scala/datastructures/priorityqueue/MaxPQ.scala
+++ src/main/scala/datastructures/priorityqueue/MaxPQ.scala
@@ -1,37 +1,37 @@
-//package datastructures.priorityqueue
-//
-//
-///**
-//  * Created by: Angel Ortega
-//  * Date: 2/18/18
-//  */
-//abstract class MaxPQ[K <: Ordered[K]] {
-//  /**
-//    * Create a priority queue with given keys
-//    */
-//
-//  /**
-//    * Insert a key into the Priority Queue
-//    */
-//  abstract def insert[B <: K](v: B)(cmp: (B, B) => Boolean): Unit
-//
-//  /**
-//    * Return and remove the largest key
-//    */
-//  abstract def delMax[B <: K](): B
-//
-//  /**
-//    * Is the priority queue empty?
-//    */
-//  abstract def isEmpty: Boolean
-//
-//  /**
-//    * Return the largest key
-//    */
-//  abstract def max[B <: K]: B
-//
-//  /**
-//    * Number of entries in the priority queue
-//    */
-////  abstract def size: Int
-//}
+package datastructures.priorityqueue
+
+
+/**
+  * Created by: Angel Ortega
+  * Date: 2/18/18
+  */
+abstract class MaxPQ[K] {
+  /**
+    * Create a priority queue with given keys
+    */
+
+  /**
+    * Insert a key into the Priority Queue
+    */
+  def insert[B <: K](v: B): Unit
+
+  /**
+    * Return and remove the largest key
+    */
+  def delMax[B <: K](): B
+
+  /**
+    * Is the priority queue empty?
+    */
+  def isEmpty: Boolean
+
+  /**
+    * Return the largest key
+    */
+  def max[B <: K]: B
+
+  /**
+    * Number of entries in the priority queue
+    */
+  def size: Int
+}
