f7b9b9d90292860fc69251e949f2d7044e6b5d3f
Angel Ortega <cs.angelortega@gmail.com>
2018-03-07 20:15:01 -0800

    Add test fixture for binary heap

--- src/test/scala/datastructures/priorityqueue/MaxBinaryHeapTest.scala
+++ src/test/scala/datastructures/priorityqueue/MaxBinaryHeapTest.scala
@@ -1,9 +1,13 @@
 package datastructures.priorityqueue
 
+import org.scalatest.{FlatSpec, Matchers}
+
 /**
   * Created by: Angel Ortega
   * Date: 3/7/2018
   */
-object MaxBinaryHeapTest {
-
+class MaxBinaryHeapTest extends FlatSpec with Matchers {
+  "Max Binary Heap Tests" should "fail" in {
+    fail()
+  }
 }
