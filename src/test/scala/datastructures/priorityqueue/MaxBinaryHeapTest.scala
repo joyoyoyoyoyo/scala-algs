f62337f1f17d9b32b833d892a0aaf06c9b0152a0
Angel Ortega <cs.angelortega@gmail.com>
2018-03-09 15:24:21 -0800

    Heap insertion tests with structured order property

--- src/test/scala/datastructures/priorityqueue/MaxBinaryHeapTest.scala
+++ src/test/scala/datastructures/priorityqueue/MaxBinaryHeapTest.scala
@@ -8,12 +8,60 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class MaxBinaryHeapTest extends FlatSpec with Matchers {
 
-  "Inserting into an empty heap" should "contain the element" in {
+  "Inserting into a heap" should "add a new element and obey the heap structure property" in {
 
-    val heap = new MaxBinaryHeap[Int](100)
+    val heap = new MaxBinaryHeap(6)
+    assert(heap.size == 0)
+    assert(heap.toString == "MaxBinaryHeap=[0, 0, 0, 0, 0, 0]")
+
     heap.insert(1)
-    println(heap)
-    fail("Complete")
+    assert(heap.size == 1)
+    assert(heap.toString == "MaxBinaryHeap=[1, 0, 0, 0, 0, 0]")
+    //  1
+    // / \
+
+    heap.insert(2)
+    assert(heap.size == 2)
+    assert(heap.toString == "MaxBinaryHeap=[2, 1, 0, 0, 0, 0]")
+    //    2
+    //   / \
+    //  1
+    // / \
+
+    heap.insert(3)
+    assert(heap.size == 3)
+    assert(heap.toString == "MaxBinaryHeap=[3, 1, 2, 0, 0, 0]")
+    //    3
+    //   / \
+    //  1   2
+    // / \ / \
+
+    heap.insert(4)
+    assert(heap.size == 4)
+    assert(heap.toString == "MaxBinaryHeap=[4, 3, 2, 1, 0, 0]")
+    //     4
+    //    / \
+    //   3   2
+    //  / \ / \
+    // 1
+
+    heap.insert(5)
+    assert(heap.size == 5)
+    assert(heap.toString == "MaxBinaryHeap=[5, 4, 2, 1, 3, 0]")
+    //     5
+    //    / \
+    //   4   2
+    //  / \ / \
+    // 1  3
+
+    heap.insert(6)
+    assert(heap.size == 6)
+    assert(heap.toString == "MaxBinaryHeap=[6, 4, 5, 1, 3, 2]")
+    //     6
+    //    / \
+    //   4   5
+    //  / \ / \
+    // 1  3 2
 
   }
 
